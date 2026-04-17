1616d2f9e0abb87381411db4ea08a042d7b4a028
alanva36 <alanva36official@gmail.com>
2026-04-17 15:29:19 +0200

    added sound

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,1 +1,15 @@
 extends Node2D
+@export var player: Player
+
+func _ready():
+	var CONSUMABLE_PATH = "res://shared/consumables/consumable_test.tscn"
+	var consumable_data = ResourceLoader.load(CONSUMABLE_PATH)
+	var consumable = consumable_data.instantiate()
+	
+	var ARTIFACT_PATH = "res://shared/consumables/artifact.tscn"
+	var artifact_data = ResourceLoader.load(ARTIFACT_PATH)
+	var artifact = artifact_data.instantiate()
+	
+	var ENEMY_PATH = "res://shared/enemy/enemy.tscn"
+	var enemy_data = ResourceLoader.load(ENEMY_PATH)
+	var enemy = enemy_data.instantiate()
